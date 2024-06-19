7e1532ffe54854902a0fb3654efa7e53169f7873
aditbangkit <adit@bangkit.com>
2024-06-20 01:14:44 +0800

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
 
         buildConfigField("String", "BASE_URL", "\"https://backend-k6kpm6mhla-et.a.run.app/\"")
         buildConfigField("String", "MODEL_URL", "\"https://ml-back-k6kpm6mhla-et.a.run.app/\"")
-        buildConfigField("String", "WEB_CLIENT_ID", "\"313139762096-ag2o620aan48cooiv5qotar13hq6f0j2.apps.googleusercontent.com\"")
-        buildConfigField("String", "CLIENT_SECRET", "\"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\"")
+        buildConfigField("String", "WEB_CLIENT_ID", "\"CLIENT\"")
+        buildConfigField("String", "CLIENT_SECRET", "\"SECRET\"")
     }
 
     buildTypes {
